b4cd863f64670fa9096ed198a7015bbf4d60fc9a
serhhin <neprokaren41@gmail.com>
2024-01-12 20:53:43 +0200

    s

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="clean_folder",   
-    version='0.0.1',
+    version='0.0.2',
     entry_points={
-        'console_sceipts':['clean-folder=clean_folder.clean:main']
+        'console_scripts':['clean-folder=clean_folder.clean:main']
     },
     packages=find_packages()
 )
